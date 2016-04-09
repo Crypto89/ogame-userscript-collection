2ba1cccc8d808bafeb044593e60b0d06e8005f8b
Jorn Wijnands <jorn.wijnands@gmail.com>
2016-04-09 19:33:53 +0200

    add manual update and fix randomness

--- gtool-idle-updater.user.js
+++ gtool-idle-updater.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GTool Idle Updater
 // @namespace    http://oplanet.eu
-// @version      0.1.2
+// @version      0.1.3
 // @description  Idle updater for GalaxyTool
 // @author       Crypto
 // @match        https://*.ogame.gameforge.com/game/*
@@ -27,12 +27,20 @@ $(document).on('giu-ticker', function (event, ms) {
     }
 });
 
+
 $(document).on('overview', function (event) {
     console.log('event: overview');
     
     var r_timeout = timeout + Math.floor(Math.random() * 300);
 
-    $("ul#menuTableTools").append('<li id="menu-giu"><span class="menu_icon"><div class="menuImage galaxy"></div></span><a href="#" class="menubutton"><span class="textLabel" style="color: #ff9600;">GIU: '+r_timeout+'</span></a></li>');
+    $("ul#menuTableTools").append(
+      '<li id="menu-giu">'+
+        '<span class="menu_icon"><div class="menuImage galaxy"></div></span>'+
+        '<a href="javascript:localStorage.setItem(\'gtiu-update\', 1);document.location.href=\'index.php?page=galaxy\'" class="menubutton">'+
+          '<span class="textLabel" style="color: #ff9600;">GIU: '+r_timeout+'</span>'+
+        '</a>'+
+      '</li>'
+    );
     
     setTimeout(function() {
         console.log('event: start-ticker');
@@ -55,10 +63,9 @@ $(document).on('galaxy', function(event) {
     for (var s=1; s<500; s++) {
         // make this slightly more random
         // generates a value for delay +- 50
-        var r_delay = delay + (Math.floor(Math.random() * 100) - 50);
         setTimeout(
             loadContent.bind(null, galaxy, s, true),
-            s * r_delay
+            s * delay + (Math.floot(Math.random() * 100) -50)
         );
     }
     
